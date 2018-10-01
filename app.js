e174fe9ecc71208cb0f9b6251a35cb2d2554f64c
delgadoJason <jason.delgado0918@gmail.com>
2018-10-01 19:55:19 -0400

    Setup template engine handlebars

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+let hbs = require('hbs');
 const app = express();
 const port = 3000;
 
+app.set('view engine', hbs);
+
 app.get('/', (req, res) => {
-	res.send('Hello world!');
+	res.render('home.hbs', {
+		pageTitle: 'Bills App',
+		intro: 'Welcome to the Bills Application'
+	});
 });
 
 app.listen(port, () => {
