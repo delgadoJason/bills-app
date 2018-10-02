831fca14e0f96d99a1c09d3934d8faac19d5c810
delgadoJason <jason.delgado0918@gmail.com>
2018-10-02 19:51:49 -0400

    Create a POST route and allow user to enter in new bill

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const app = express();
+let bodyParser = require('body-parser');
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+let bills = [
+		{
+			companyName: 'Duke Energy',
+			amountDue: 125.28,
+			dueDate: 'Sep 13, 2018'
+		}
+];
 
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/bills', (req, res) => {
-	let bills = [
-		{
-			companyName: 'Duke Energy',
-			amountDue: 125.28,
-			dueDate: 'Sep 13, 2018'
-		}
-	]
 	res.render('bills', {bills: bills});
+});
+
+app.post('/bills', (req, res) => {
+	let company = req.body.company;
+	let amount = req.body.amount;
+	let dueDate = req.body.dueDate;
+	let newBill = {
+		companyName: company,
+		amountDue: amount,
+		dueDate
+	};
+	bills.push(newBill);
+	res.redirect('/bills');
+});
+
+app.get('/bills/new', (req, res) => {
+	res.render('new-bill');
 });
 
 app.listen(port, () => {
