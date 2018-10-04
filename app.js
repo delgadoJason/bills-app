2d48cf3859cd02351c71e121c13c98430d17a47b
delgadoJason <jason.delgado0918@gmail.com>
2018-10-03 21:05:44 -0400

    Add a database for persistant data

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
 const express = require('express');
 const app = express();
 let bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = 3000;
 
+mongoose.connect('mongodb://localhost:27017/bill-tracker');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
-let bills = [
-		{
-			companyName: 'Duke Energy',
-			typeOfBill: 'Power Bill',
-			amountDue: 125.28,
-			dueDate: 'Sep 13, 2018'
-		},
-		{
-			companyName: 'Crestridge Utilities, LLC',
-			typeOfBill: 'Water Bill',
-			amountDue: 47.82,
-			dueDate: 'Sep 24, 2018'
-		}, 
-		{
-			companyName: 'Spectrum',
-			typeOfBill: 'Cable Bill',
-			amountDue: 136.85,
-			dueDate: 'Today'
-		}
-];
+// Bills Scehma
+const billsSchema = new mongoose.Schema({
+	companyName: String,
+	typeOfBill: String,
+	amountDue: Number,
+	dueDate: Date
+});
+
+let Bills = mongoose.model('Bill', billsSchema);
+
+// let bills = [
+// 		{
+// 			companyName: 'Duke Energy',
+// 			typeOfBill: 'Power Bill',
+// 			amountDue: 125.28,
+// 			dueDate: 'Sep 13, 2018'
+// 		},
+// 		{
+// 			companyName: 'Crestridge Utilities, LLC',
+// 			typeOfBill: 'Water Bill',
+// 			amountDue: 47.82,
+// 			dueDate: 'Sep 24, 2018'
+// 		}, 
+// 		{
+// 			companyName: 'Spectrum',
+// 			typeOfBill: 'Cable Bill',
+// 			amountDue: 136.85,
+// 			dueDate: 'Today'
+// 		}
+// ];
 
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/bills', (req, res) => {
-	res.render('bills', {bills: bills});
+	Bills.find({}, (err, bills) => {
+		if(err) {
+			res.send('There was an error!', err);
+		} else {
+			res.render('bills', {bills: bills});
+		}
+	});
 });
 
 app.post('/bills', (req, res) => {
@@ -46,7 +64,8 @@ app.post('/bills', (req, res) => {
 		amountDue: amount,
 		dueDate
 	};
-	bills.push(newBill);
+	let bill = new Bills(newBill);
+	bill.save();
 	res.redirect('/bills');
 });
 
